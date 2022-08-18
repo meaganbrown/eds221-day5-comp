74ff8d33eec4ac6826f481ff388960bf5808ce67
Meagan Brown <meaganbrown15@gmail.com>
2022-08-18 09:34:07 -0700

    .

---
+++ src/max_sustainable_harvest.R
@@ -0,0 +1,15 @@
+#' Calculate maximum sustainable harvest
+#'
+#' @param K carrying capacity for the population (in individuals)
+#' @param r intrinsic growth rate of the fisher ( 1 / yr)
+#'
+#' @return harvest amount (individuals)
+#' @export 
+#'
+#' @examples
+#' max_sustainable_yield(K = 3000, r = 0.52)
+
+max_sustainable_harvest <- function(K, r) {
+  harvest <- (K * r) / 4
+  return(harvest)
+}
